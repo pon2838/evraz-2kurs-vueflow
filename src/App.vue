62d878eb074b94ca3624816858704db3bf5284c4
Pavel <im.lucky.jr@gmail.com>
2024-05-02 16:13:08 +0500

    start

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import { ref } from 'vue';
+import {
+    VueFlow,
+    useVueFlow,
+} from '@vue-flow/core';
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+const initialNodes = ref([
+    {
+        id:       '1',
+        position: { x: 50, y: 50 },
+        label:    'Node 1',
+    },
+]);
+const { addNodes, removeNodes } = useVueFlow();
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+function generateRandomNode() {
+    return {
+        id:       Math.random().toString(),
+        position: { x: Math.random() * 500, y: Math.random() * 500 },
+        label:    'Random Node',
+    };
+}
+
+function onAddNode() {
+    // add a single node to the graph
+    addNodes(generateRandomNode());
+}
+
+function onAddNodes() {
+    // add multiple nodes to the graph
+    addNodes(Array.from({ length: 10 }, generateRandomNode));
+}
+
+// remove a single node from the graph
+function onRemoveNode() {
+    removeNodes('1')
+}
+
+// remove multiple nodes from the graph
+function onRemoveNodes() {
+    removeNodes(['1', '2'])
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+    <VueFlow :nodes="initialNodes"/>
+    
+    <button
+        type="button"
+        @click="onAddNode"
+    >Add a node
+    </button>
+    <button
+        type="button"
+        @click="onAddNodes"
+    >Add multiple nodes
+    </button>
+    <button
+        type="button"
+        @click="onRemoveNode"
+    >Remove a node
+    </button>
+</template>
